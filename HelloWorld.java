95d0ad3377c04c1fbecc15fee8c46a5d9b88af4f
sami7187 <79498729+sami7187@users.noreply.github.com>
2021-03-13 18:34:29 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String[] args) {
 	 //This is simple HelloWorld 
-	    System.out.println("Hello World Feb/March/April 2021 Webhook - whenever we commit a code ");
+	    System.out.println("Hello World Feb/March/April 2021 Webhook - whenever we commit a code jenkins will immediatedly trigger ");
 		
 	 }
 }
